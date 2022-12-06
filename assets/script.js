76eda2996626aa200d618d12b68c7e0bd7f141b2
James Harrington <jamesharrington585@hotmail.com>
2022-12-05 21:17:26 -0500

    deleting button now removes element from array. array can now pass to the api call to narrow recipe search

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ var btn = document.getElementById("#btn");
 var recipeSearch = document.getElementById("#search");
 var result = document.getElementById("#result-div");
 var ingredientList = document.getElementById("#ingredient-div");
+var submitBtn = document.getElementById("#submit-button")
 
 // API key and id (should encrypt if possible)
 
@@ -13,17 +14,6 @@ var ingredientArr = [];
 
 // click event to make API call
 
-// (commented out while i work on this repurposing this feature)
-
-// btn.addEventListener("click", function(event) {
-//     event.preventDefault();
-//     var ingredient = recipeSearch.value;
-//     console.log(ingredient);
-
-//     fetchRecipe(ingredient);
-// });
-
-
 
 // function to accumulate user's desired ingredients to be stored in an array and used to narrow down recipe results
 
@@ -32,31 +22,50 @@ btn.addEventListener("click", function(event) {
     var ingredient = recipeSearch.value;
     console.log(ingredient);
 
-    //creat ingredient buttons
+    //create ingredient buttons
 
     var ingrediBtn = document.createElement("button");
     ingrediBtn.textContent = ingredient;
     ingredientList.appendChild(ingrediBtn);
+
+    // remove buttons on click
+
     ingrediBtn.addEventListener("click", function(event) {
         var ingr = event.target;
+
+        // remove ingredients from array
+
+        ingredientArr = ingredientArr.filter(function (buttonItem) {
+
+            return buttonItem !== ingr.textContent;
+        });
+
         ingr.remove();
+        
     });
     
-
     //push ingredients into array
 
     ingredientArr.push(ingredient);
     console.log(ingredientArr);
-    // localStorage.setItem('ingredients', JSON.stringify(ingredientArr));
+    
     recipeSearch.value = "";
-
 
 });
 
+function storeRecipe() {
+    localStorage.setItem('FaveRecipes', JSON.stringify(ingredientArr));
+};
+
+
+
 // API call
 
-function fetchRecipe(ingredient){   
-    let api = `https://api.edamam.com/search?app_id=${id}&app_key=${key}&q=${ingredient}`;
+function fetchRecipe(){   
+
+    storeRecipe();
+
+    let api = `https://api.edamam.com/search?app_id=${id}&app_key=${key}&q=${ingredientArr}`;
     fetch(api)
     .then(function(response){
         let data = response.json();
@@ -78,10 +87,13 @@ function fetchRecipe(ingredient){
             newRecipe.innerHTML  = 
             `<div>
             <h2>${data.hits[i].recipe.label}</h2>
-            <a href="${data.hits[i].recipe.url}"><img src="${data.hits[1].recipe.image}" /></a>
-            <img src="${data.hits[i].recipe.image}"/>
+            <a href="${data.hits[i].recipe.url}">
+            <img src="${data.hits[i].recipe.image}" /></a>
             </div>`;
             result.appendChild(newRecipe);
         };
     });
 };
+
+
+submitBtn.addEventListener("click", fetchRecipe);
